b3a452596c7f76e4883798fbcf2a099185855cfe
Prashishgt <prashish24@gmail.com>
2023-08-24 22:14:08 +0545

    initlized project in redux to handle async feature

--- 03AsyncWithAxios/src/App.jsx
+++ 03AsyncWithAxios/src/App.jsx
@@ -1,5 +1,13 @@
+import AddPostForm from "./features/posts/AddPostForm";
+import PostsList from "./features/posts/PostsList";
+
 function App() {
-  return <></>;
+  return (
+    <main className="App">
+      <PostsList />
+      <AddPostForm />
+    </main>
+  );
 }
 
 export default App;
